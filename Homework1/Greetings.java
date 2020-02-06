e516c503487eb33b611faaa1f546c44a63a63305
kiridmi <60718071+kiridmi@users.noreply.github.com>
2020-02-06 20:46:42 +0200

    Add files via upload

---
+++ Homework1/Greetings.java
@@ -0,0 +1,11 @@
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		String name = "Dmitrijs";
+		int age = 30;
+		
+		System.out.println("Sveiki, mani sauc " + name + " esmu " + age + " gadus jauns!");
+	}
+
+}
